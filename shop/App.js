17a88ad6738e31fe078c0cdeff93f78749a43afa
Konyaeva Olga <oly3622@yandex.ru>
2022-10-26 13:41:08 +0300

    first commit

--- shop/App.js
+++ shop/App.js
@@ -1,21 +1,23 @@
+
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Ноутбук', price: 75000},
+    {id: 2, title: 'Мышка', price: 3000},
+    {id: 3, title: 'Клавиатура', price: 12000},
+    {id: 4, title: 'Геймпад', price: 6700},
+
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, img) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <p>${price} руб.</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title,item.price));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
